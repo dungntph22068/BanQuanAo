694bfee9c797e46d59c4efeb1d41f307d96dc9e2
tie8dun6 <tiendung6529@gmail.com>
2023-07-27 01:16:29 +0700

    code java

---
+++ src/main/java/com/example/demo/model/AppUser.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "appuser")
+@Entity
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userid")
+    private Long userId;
+
+    @Column(name = "username")
+    private String userName;
+
+    @Column(name = "Encryted_Password")
+    private String encrytedPassword;
+
+    @Column(name = "Enabled")
+    private boolean enabled;
+
+    @Column(name = "Email")
+    private String email;
+}
